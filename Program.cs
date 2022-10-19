92af3ab4bdca0a1d7bf2da30c549aa9801eda3fa
Frederick Lopez <mtester010@gmail.com>
2022-10-19 18:18:30 -0500

    Follow the instruction to have the strategy approach design pattern

--- Program.cs
+++ Program.cs
@@ -1,2 +1,78 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public abstract class Client
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public int? Age { get; set; }
+    public bool accessDisabled { get; set; }
+
+    public AccessHandler AccessHandler { get; set; }
+
+    public virtual void HandleAccess()
+    {
+        AccessHandler.getAccess();
+    }
+}
+
+public class User : Client
+{
+    public int Reputation { get; set; }
+    public AccessHandler AccessHandler { get; set; }
+
+    public User(int reputation)
+    {
+        AccessHandler = new HasReputation();
+        AccessHandler.getAccess(reputation);
+    }
+
+    
+}
+
+public class Manager : Client
+{
+    public AccessHandler AccessHandler { get; set; }
+    public Manager()
+    {
+        this.AccessHandler = new HasAccessAutomatic();
+    }
+}
+public class Admin : Client
+{
+    public Admin()
+    {
+        this.AccessHandler = new HasAccessAutomatic();
+    }
+}
+
+public interface AccessHandler
+{
+    public bool getAccess(int? Reputation = 0, bool accessDisabled = false);
+}
+
+public class HasReputation : AccessHandler
+{
+    public bool getAccess(int? Reputation = 0, bool accessDisabled = false)
+    {
+        if(Reputation > 20)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+public class HasAccessAutomatic : AccessHandler
+{
+    public bool getAccess(int? Reputation = 0, bool accessDisabled = false)
+    {
+        if(accessDisabled == false)
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+}
